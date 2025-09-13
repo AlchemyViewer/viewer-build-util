76d6c9b12eeed9d8692a4f2cfe287e2f0473f611
Rye <rye@alchemyviewer.org>
2025-09-13 05:08:44 -0400

    Rework macOS signing for Alchemy app layout and libs

--- sign-pkg-mac/sign.sh
+++ sign-pkg-mac/sign.sh
@@ -5,18 +5,12 @@
 # cert_base64: base64-encoded signing certificate
 # cert_name: full name of signing certificate
 # cert_pass: signing certificate password
-# note_user: notarization username
-# note_pass: password for note_user
-# note_team: Team ID for note_user
 
 mydir="$(dirname "$0")"
 app_path="$1"
 
-# shellcheck disable=SC1091
-. "$mydir/retry_loop"
-
 gotall=true
-for var in app_path cert_base64 cert_name cert_pass note_user note_pass note_team
+for var in app_path cert_base64 cert_name cert_pass
 do
     if [[ -z "${!var}" ]]
     then
@@ -32,7 +26,6 @@ set -x -e
 # ****************************************************************************
 # The following is derived from
 # https://federicoterzi.com/blog/automatic-code-signing-and-notarization-for-macos-apps-using-github-actions/
-# shellcheck disable=SC2154
 base64 --decode > certificate.p12 <<< "$cert_base64"
 
 # We need to create a new keychain, otherwise using the certificate will prompt
@@ -45,118 +38,98 @@ echo "::add-mask::$keychain_pass"
 set -x
 sleep 1
 security create-keychain -p "$keychain_pass" viewer.keychain
+security set-keychain-settings -lut 21600 viewer.keychain
 security default-keychain -s viewer.keychain
 security unlock-keychain -p "$keychain_pass" viewer.keychain
-# shellcheck disable=SC2154
-security import certificate.p12 -k viewer.keychain -P "$cert_pass" \
-         -T /usr/bin/codesign
-security set-key-partition-list -S 'apple-tool:,apple:,codesign:' -s \
-         -k "$keychain_pass" viewer.keychain
+security import certificate.p12 -k viewer.keychain -P "$cert_pass" -T /usr/bin/codesign
+security set-key-partition-list -S 'apple-tool:,apple:,codesign:' -s -k "$keychain_pass" viewer.keychain
 rm certificate.p12
 
-# ****************************************************************************
-#   sign executables
-# ****************************************************************************
-# arrange to retry signing, since empirically this is a
-# low-reliability operation
-retries=3
-signwait=15
-function signloop() {
-    # save +x / -x state and suppress
-    xtrace="$(set +o | grep xtrace)"
-    set +x
-    # shellcheck disable=SC2064
-    trap "$xtrace" RETURN
+# We sign from the inside out
 
-    local exe
-    # we pass the executable to sign as the last argument
-    # shellcheck disable=SC1083
-    eval exe=\${$#}
-    exe="$(basename "$exe")"
-    retry_loop "$exe signing" $retries $signwait /usr/bin/codesign "$@"
-}
+# Plugin bundle
+plugin_path="$app_path/Contents/Resources/SLPlugin.app"
+plugin_contents="$plugin_path/Contents"
 
-resources="$app_path/Contents/Resources"
-# plain signing
+# VLC plugin
 for signee in \
-    "$resources"/*.dylib \
-    "$resources"/llplugin/*.dylib \
-    "$app_path/Contents/Frameworks/Chromium Embedded Framework.framework/Libraries"/*.dylib
+    "$plugin_contents"/Frameworks/libvlccore.dylib \
+    "$plugin_contents"/Frameworks/libvlccore.9.dylib \
+    "$plugin_contents"/Frameworks/libvlc.dylib \
+    "$plugin_contents"/Frameworks/libvlc.5.dylib \
+    "$plugin_contents"/Frameworks/plugins/*.dylib \
+    "$plugin_contents"/Frameworks/plugins/*.dat \
+    "$plugin_contents"/Frameworks/media_plugin_libvlc.dylib
 do
-    # shellcheck disable=SC2154
-    signloop --force --timestamp --keychain viewer.keychain \
+    codesign --verbose --force --timestamp --keychain viewer.keychain \
              --sign "$cert_name" "$signee"
 done
-# deep signing
+
+# CEF plugin
 for signee in \
-    "$resources/updater/SLVersionChecker" \
-    "$resources/SLPlugin.app/Contents/MacOS/SLPlugin" \
-    "$resources/SLVoice" \
-    "$app_path"
+    "$plugin_contents/Frameworks/Chromium Embedded Framework.framework/Libraries"/*.dylib \
+    "$plugin_contents/Frameworks/Chromium Embedded Framework.framework/Resources"/*.bin \
+    "$plugin_contents/Frameworks/Chromium Embedded Framework.framework" \
+    "$plugin_contents"/Frameworks/media_plugin_cef.dylib
 do
-    signloop --verbose --deep --force \
+    codesign --verbose --force --timestamp --keychain viewer.keychain \
+             --sign "$cert_name" "$signee"
+done
+
+# DullahanHelper and SLPlugin
+for signee in \
+    "$plugin_contents/Frameworks"/DullahanHelper*.app \
+    "$plugin_path"
+do
+    codesign --verbose --force --timestamp \
              --entitlements "$mydir/installer/slplugin.entitlements" \
              --options runtime --keychain viewer.keychain \
              --sign "$cert_name" "$signee"
 done
 
-spctl -a -texec -vvvv "$app_path"
+# Resources
+resources="$app_path/Contents/Resources"
 
-# ****************************************************************************
-#   notarize the app
-# ****************************************************************************
-# We can't notarize an app bundle directly, but we need to compress it as an
-# archive. Therefore, we create a zip file containing our app bundle, so that
-# we can send it to the notarization service. Kind of funny to do this when
-# actions/download-archive just downloaded and unpacked the artifact .zip
-# file, but oh well.
-echo "Creating temp notarization archive"
-app_base="$(basename "$app_path")"
-# Don't put the zipfile in the same sparseimage we're trying to sign and
-# notarize! That would require creating the sparseimage with extra room just
-# for the .zip, and there's no point.
-zip_file="$RUNNER_TEMP/${app_base/.app/.zip}"
-ditto -c -k --keepParent "$app_path" "$zip_file"
-if [[ ! -f "$zip_file" ]]
-then
-    echo "Notarization error: ditto failed"
-    exit 1
-fi
-# shellcheck disable=SC2154,SC2064
-trap "rm '$zip_file'" EXIT
+# SLVoice Libs
+for signee in \
+    "$resources"/libortp.dylib \
+    "$resources"/libvivoxsdk.dylib
+do
+    codesign --verbose --force --timestamp --keychain viewer.keychain \
+             --sign "$cert_name" "$signee"
+done
 
-credentials=(--apple-id "$note_user" --password "$note_pass" --team-id "$note_team")
+# SLVoice binary
+for signee in \
+    "$resources/SLVoice"
+do
+    codesign --verbose --force --timestamp \
+             --entitlements "$mydir/installer/slplugin.entitlements" \
+             --options runtime --keychain viewer.keychain \
+             --sign "$cert_name" "$signee"
+done
 
-# Here we send the notarization request to Apple's Notarization service,
-# waiting for the result. This typically takes a few seconds inside a CI
-# environment, but it might take more depending on the App characteristics.
-# Visit the Notarization docs for more information and strategies on how to
-# optimize it if you're curious.
-echo "Notarize app"
-# emit notarytool output to stderr in real time but also capture in variable
-set +e
-output="$(xcrun notarytool submit "$zip_file" --wait \
-          "${credentials[@]}" 2>&1 | \
-          tee /dev/stderr ; \
-          exit "${PIPESTATUS[0]}")"
-# Without the final 'exit' above, we'd be checking the rc from 'tee' rather
-# than 'notarytool'.
-rc=$?
-set +x
-[[ "$output" =~ 'id: '([^[:space:]]+) ]]
-match=$?
-set -x
-# Run notarytool log if we find an id: anywhere in the output, regardless of
-# rc: notarytool can terminate with rc 0 even if it fails.
-if [[ $match -eq 0 ]]
-then
-    xcrun notarytool log "${BASH_REMATCH[1]}" "${credentials[@]}"
-fi
-[[ $rc -ne 0 ]] && exit $rc
-set -e
+# App Frameworks
+frameworks="$app_path/Contents/Frameworks"
+for signee in \
+    "$frameworks"/libopenal.dylib \
+    "$frameworks"/libalut.dylib \
+    "$frameworks"/libfmod.dylib \
+    "$frameworks"/libdiscord_partner_sdk.dylib \
+    "$frameworks"/libndofdev.dylib \
+    "$frameworks"/libSDL3.dylib \
+    "$frameworks"/libllwebrtc.dylib
+do
+    codesign --verbose --force --timestamp --keychain viewer.keychain \
+             --sign "$cert_name" "$signee"
+done
 
-# Finally, we need to "attach the staple" to our executable, which will allow
-# our app to be validated by macOS even when an internet connection is not
-# available.
-echo "Attach staple"
-xcrun stapler staple "$app_path"
+# App Signing
+for signee in \
+    "$app_path"
+do
+    codesign --verbose --force --timestamp \
+             --entitlements "$mydir/installer/slplugin.entitlements" \
+             --options runtime --keychain viewer.keychain \
+             --sign "$cert_name" "$signee"
+done
